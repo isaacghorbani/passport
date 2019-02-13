fd039a1dbf901245593d3393673162198d7e51f0
isaacghorbani <35466710+isaacghorbani@users.noreply.github.com>
2019-02-13 15:00:40 +0330

    Update UserRepository.php
    Make Multi Auth

--- src/Bridge/UserRepository.php
+++ src/Bridge/UserRepository.php
@@ -81,4 +81,32 @@ class UserRepository implements UserRepositoryInterface
         }
         return new User($user->getAuthIdentifier());
     }
+    
+    public function getEntityByUserCredentials($username, $password, $grantType, ClientEntityInterface $clientEntity,$provider)
+    {
+
+        $provider = config('auth.guards.'.$provider.'.provider');
+
+        if (is_null($model = config('auth.providers.'.$provider.'.model'))) {
+            throw new RuntimeException('Unable to determine authentication model from configuration.');
+        }
+
+        if (method_exists($model, 'findForPassport')) {
+            $user = (new $model)->findForPassport($username);
+        } else {
+            $user = (new $model)->where('email', $username)->first();
+        }
+
+        if (! $user) {
+            return;
+        } elseif (method_exists($user, 'validateForPassportPasswordGrant')) {
+            if (! $user->validateForPassportPasswordGrant($password)) {
+                return;
+            }
+        } elseif (! $this->hasher->check($password, $user->getAuthPassword())) {
+            return;
+        }
+
+        return new User($user->getAuthIdentifier());
+    }
 }
